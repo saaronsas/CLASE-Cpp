bf50bebca25cbea0fbac1c8220bd736a7ebd849e
saaronsas <55530865+saaronsas@users.noreply.github.com>
2020-02-13 18:45:59 +0100

    Update OrdenarVocales.cpp

--- 29.OrdenarVocales/OrdenarVocales.cpp
+++ 29.OrdenarVocales/OrdenarVocales.cpp
@@ -68,3 +68,61 @@ void imprimirPalabra(){
 	}
 	
 }
+
+/*#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+struct Palabra{
+	char palabra[30];
+	int vocales=0;
+}voc[4];
+
+int main(){
+	char vocales[]={'a','e','i','o','u'}, aux;
+	Palabra x;
+	for(int i=0; i<4; i++){
+		cout<<"Palabra["<<i+1<<"]:  "; cin.getline(voc[i].palabra,30,'\n');
+	}
+	system("cls");
+	for(int i=0; i<4; i++){
+		for(int j=0; j<strlen(voc[i].palabra); j++){
+			for(int z=0; z<5; z++){
+				if(voc[i].palabra[j]==vocales[z]){
+					voc[i].vocales++;
+				}
+			}
+		}
+	}
+	
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			if(voc[j].vocales>voc[j+1].vocales){
+				aux = voc[j].vocales;
+				voc[j].vocales=voc[j+1].vocales;
+				voc[j+1].vocales=aux;
+			}
+		}
+	}
+	for(int i=0; i<4; i++){
+		for(int j=0; j<3; j++){
+			if(voc[j].vocales>voc[j].vocales){
+				x = voc[j];
+				voc[j]=voc[j+1];
+				voc[j+1]=x;
+			}
+		/*cout<<"Posicion "<<i+1<<".\n";
+		cout<<"\tPalabra: "<<voc[i].palabra<<endl;
+		cout<<"\tNumero Vocales: "<<voc[i].vocales<<endl<<endl;*/
+		}
+	}
+	for(int i=0; i<4; i++){
+		cout<<voc[i].palabra<<" ";
+	}
+	
+	getch();
+	return 0;
+}*/
